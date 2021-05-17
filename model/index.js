10500fd097a0742610e35aca3c317ff030df78e4
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-05-17 22:41:51 +0300

    Add Homework-2

--- model/index.js
+++ model/index.js
@@ -1,22 +1,154 @@
-// const fs = require('fs/promises');
-const contacts = require('./contacts.json');
+const fs = require('fs/promises');
+const Joi = require('joi');
+const path = require('path');
+const { v4: uuid } = require('uuid');
+
+const contactsPath = path.join(__dirname, './contacts.json');
 
 const listContacts = async (req, res) => {
-  res.status(200).json(contacts);
+  const data = await fs.readFile(contactsPath, 'utf-8');
+  const parseContacs = JSON.parse(data);
+  res.send(parseContacs);
 };
 
-const getContactById = async (contactId, req, res) => {};
+const getContactById = async (req, res, next) => {
+  try {
+    const { contactId } = req.params;
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const parseContacs = JSON.parse(data);
+    const contact = parseContacs.find(contact => contact.id === contactId);
+    res.json({
+      status: 'success',
+      data: { contact },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
-// const removeContact = async contactId => {};
+const removeContact = async (req, res, next) => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const parseContacs = JSON.parse(data);
+    const newContacts = parseContacs.filter(
+      contact => contact.id !== req.params.contactId,
+    );
 
-// const addContact = async body => {};
+    const newContactsJson = JSON.stringify(newContacts);
 
-// const updateContact = async (contactId, body) => {};
+    fs.writeFile(contactsPath, newContactsJson);
 
+    res.json({
+      status: 'success',
+      data: {},
+      message: 'COntact delete success',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+const addContact = async (req, res, next) => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const parseContacs = JSON.parse(data);
+    const newContact = {
+      id: uuid(),
+      ...req.body,
+    };
+
+    const newContacts = [...parseContacs, newContact];
+
+    const newContactsJson = JSON.stringify(newContacts);
+
+    fs.writeFile(contactsPath, newContactsJson);
+
+    res.json({
+      status: 'success',
+      data: { newContact },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const updateContact = async (req, res, next) => {
+  try {
+    const data = await fs.readFile(contactsPath, 'utf-8');
+    const parseContacs = JSON.parse(data);
+    const contactToChange = parseContacs.find(contact => {
+      return contact.id === req.params.contactId;
+    });
+
+    const newContacts = parseContacs.map(contact => {
+      if (contact.id === contactToChange.id) {
+        return (contact = {
+          id: contactToChange.id,
+          ...req.body,
+        });
+      }
+      return contact;
+    });
+
+    const newContactsJson = JSON.stringify(newContacts);
+    fs.writeFile(contactsPath, newContactsJson);
+    res.json({
+      statuss: 'success',
+      data: {
+        contact: {
+          id: contactToChange.id,
+          ...req.body,
+        },
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+const validateCreateContact = (req, res, next) => {
+  const createContactRules = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().required(),
+    phone: Joi.string().required(),
+  });
+
+  const validationResult = createContactRules.validate(req.body);
+
+  if (validationResult.error) {
+    const newError = new Error();
+    newError.status = 400;
+    throw newError;
+  }
+
+  next();
+};
+
+const validateUpdateContact = (req, res, next) => {
+  const updateContactRules = Joi.object({
+    name: Joi.string(),
+    email: Joi.string(),
+    phone: Joi.string(),
+  });
+
+  const validBodyReq = req.body.name || req.body.email || req.body.phone;
+
+  const validationResult = updateContactRules.validate(req.body);
+
+  if (!validBodyReq || validationResult.error) {
+    const newError = new Error();
+    newError.status = 400;
+    newError.type = 'UpdateContact';
+    throw newError;
+  }
+
+  next();
+};
 module.exports = {
   listContacts,
   getContactById,
-  // removeContact,
-  // addContact,
-  // updateContact,
+  removeContact,
+  addContact,
+  updateContact,
+  validateCreateContact,
+  validateUpdateContact,
 };
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,58 +1,23 @@
 const express = require('express');
 const router = express.Router();
-const { listContacts } = require('../../model/index.js');
-let contacts = require('../../model/contacts.json');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  updateContact,
+  removeContact,
+  validateCreateContact,
+  validateUpdateContact,
+} = require('../../model/index');
 
 router.get('/', listContacts);
 
-router.get('/:contactId', (req, res, next) => {
-  const { contactId } = req.params;
-  const contact = contacts.filter(el => el.id === contactId);
-  console.log(contact);
-  res.json({
-    status: 'success',
-    code: 200,
-    data: { contact },
-  });
-});
+router.get('/:contactId', getContactById);
 
-router.post('/', (req, res, next) => {
-  const { name, email, phone } = req.body;
-  const contact = {
-    id: 22,
-    name,
-    email,
-    phone,
-  };
+router.post('/', validateCreateContact, addContact);
 
-  contacts.push(contact);
+router.delete('/:contactId', removeContact);
 
-  res.status(201).json({
-    status: 'success',
-    code: 201,
-    data: { contact },
-  });
-});
-
-router.delete('/:contactId', (req, res, next) => {
-  const { contactId } = req.params;
-  const newcontacts = contacts.filter(el => el.id !== contactId);
-  contacts = [...newcontacts];
-  res.status(204).json();
-});
-
-router.put('/:contactId', (req, res, next) => {
-  const { contactId } = req.params;
-  const { name, email, phone } = req.body;
-  const [contact] = contacts.filter(el => el.id === contactId);
-  contact.name = name;
-  contact.email = email;
-  contact.phone = phone;
-  res.json({
-    status: 'success',
-    code: 200,
-    data: { contact },
-  });
-});
+router.put('/:contactId', validateUpdateContact, updateContact);
 
 module.exports = router;

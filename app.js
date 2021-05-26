451536378544122f32b8c553ded87c566279f38a
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-05-26 23:39:08 +0300

    Add homework 3

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
-// const mongoose = require('mongoose');
 require('dotenv').config();
 
 const contactsRouter = require('./routes/api/contacts');
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -5,19 +5,27 @@ const {
   getContactById,
   addContact,
   updateContact,
+  updateStatusContact,
   removeContact,
   validateCreateContact,
   validateUpdateContact,
+  validateId,
 } = require('../../model/index');
 
 router.get('/', listContacts);
 
-router.get('/:contactId', getContactById);
+router.get('/:contactId', validateId, getContactById);
 
 router.post('/', validateCreateContact, addContact);
 
-router.delete('/:contactId', removeContact);
+router.delete('/:contactId', validateId, removeContact);
 
-router.put('/:contactId', validateUpdateContact, updateContact);
+router.put('/:contactId', validateId, validateUpdateContact, updateContact);
+router.patch(
+  '/:contactId/favorite',
+  validateId,
+  validateUpdateContact,
+  updateStatusContact,
+);
 
 module.exports = router;

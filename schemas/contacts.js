65775bc9fbc3c50c572f64b56508d67cc3bdaa7d
Yaroslaw Dmukhovsckyi <ermacus096@gmail.com>
2021-06-04 15:52:36 +0300

    Finish homework 4

--- schemas/contacts.js
+++ schemas/contacts.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const { Schema } = mongoose;
+const { Schema, SchemaTypes } = mongoose;
 
 const contactSchema = new Schema(
   {
@@ -21,6 +21,10 @@ const contactSchema = new Schema(
     favorite: {
       type: Boolean,
       default: false,
+    },
+    owner: {
+      type: SchemaTypes.ObjectId,
+      ref: 'user',
     },
   },
   { versionKey: false, timestamps: true },
---
+++ schemas/users.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const SALT_FACTOR = 6;
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Email is required'],
+    unique: true,
+  },
+  subscription: {
+    type: String,
+    enum: ['starter', 'pro', 'business'],
+    default: 'starter',
+  },
+  token: {
+    type: String,
+    default: null,
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified) return next();
+  this.password = await bcrypt.hash(
+    this.password,
+    bcrypt.genSaltSync(SALT_FACTOR)
+  );
+});
+userSchema.methods.isValidPassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
